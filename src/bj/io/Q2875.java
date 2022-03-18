b609c501949b3ca83725fe7e60bc3f2b4e627810
inhalin <inhalin.kim@gmail.com>
2022-03-18 18:23:28 +0900

    2751 2875 8393 10818

---
+++ src/bj/io/Q2875.java
@@ -0,0 +1,25 @@
+package bj.io;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+//https://www.acmicpc.net/problem/2875
+//대회 or 인턴
+public class Q2875 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int count = 0;
+        while (n >= 2 && m >= 1 && n + m >= 3 + k) {
+            n -= 2;
+            m--;
+            count++;
+        }
+        System.out.println(count);
+    }
+}
