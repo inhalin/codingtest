678bc65f30358bf4528ea98a23139953ca673334
inhalin <inhalin.kim@gmail.com>
2022-03-21 17:19:30 +0900

    Leetcode Arrays101 Conclusion

---
+++ src/leetcode/arrays/FindDiappearedNumbers.java
@@ -0,0 +1,27 @@
+package leetcode.arrays;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FindDiappearedNumbers {
+    public static void main(String[] args) {
+        int[] a = {4,3,2,7,8,2,3,1};
+        int[] a2 = {1,1};
+        System.out.println(findDisappearedNumbers(a));
+        System.out.println(findDisappearedNumbers(a2));
+    }
+
+    public static List<Integer> findDisappearedNumbers(int[] nums) {
+        Arrays.sort(nums);
+        List<Integer> res = new ArrayList<>();
+
+        for (int i = 1; i <= nums.length; i++) {
+            if (Arrays.binarySearch(nums, i) < 0) {
+                res.add(i);
+            }
+        }
+
+        return res;
+    }
+}
