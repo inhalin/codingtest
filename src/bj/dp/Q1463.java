99258914b4a091662118043aacd9f8e0f7e33362
inhalin <inhalin.kim@gmail.com>
2022-04-05 15:35:58 +0900

    bj workbook 10475 dp

--- src/bj/dp/Q1463.java
+++ src/bj/dp/Q1463.java
@@ -7,18 +7,21 @@ import java.io.InputStreamReader;
 //https://www.acmicpc.net/problem/1463
 //1로 만들기
 public class Q1463 {
-    static Integer[] dp;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        dp = new Integer[n + 1];
-        dp[0] = dp[1] = 0;
+
         System.out.println(recur(n));
         System.out.println(recur(n, 0));
+        System.out.println(bottomUp(n));
     }
 
     static int recur(int n) {
+        Integer[] dp;
+
+        dp = new Integer[n + 1];
+        dp[0] = dp[1] = 0;
+
         if (dp[n] == null) {
             if (n % 6 == 0) {
                 dp[n] = Math.min(recur(n - 1), Math.min(recur(n / 3), recur(n / 2))) + 1;
@@ -30,6 +33,7 @@ public class Q1463 {
                 dp[n] = recur(n - 1) + 1;
             }
         }
+
         return dp[n];
     }
 
@@ -37,5 +41,18 @@ public class Q1463 {
         if (n < 2) return count;
 
         return Math.min(recur(n / 2, count + 1 + (n % 2)), recur(n / 3, count + 1 + n % 3));
+    }
+
+    static int bottomUp(int n) {
+        int[] bu = new int[n + 1];
+        bu[0] = bu[1] = 0;
+
+        for (int i = 2; i <= n; i++) {
+            bu[i] = bu[i-1] + 1;
+            if (i % 2 == 0) bu[i] = Math.min(bu[i], bu[i/2] + 1);
+            if (i % 3 == 0) bu[i] = Math.min(bu[i], bu[i/3] + 1);
+        }
+
+        return bu[n];
     }
 }
---
+++ src/bj/dp/Q2579.java
@@ -0,0 +1,28 @@
+package bj.dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Q2579 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] step = new int[n + 1];
+        int[] dp = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            step[i] = Integer.parseInt(br.readLine());
+        }
+
+        dp[1] = step[1];
+        dp[2] = step[1] + step[2];
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = Math.max(dp[i - 2], dp[i - 3] + step[i - 1]) + step[i];
+        }
+
+        System.out.println(Arrays.toString(dp));
+    }
+}
