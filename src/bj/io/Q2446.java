f66f1ff78befec35164470edbe0f581ff1b10c07
inhalin <inhalin.kim@gmail.com>
2022-04-08 01:32:45 +0900

    bj io 2442 2445 2446

--- src/bj/io/Q2446.java
+++ src/bj/io/Q2446.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 
 //https://www.acmicpc.net/problem/2438
 //별 찍기 - 1
-public class Q2442 {
+public class Q2446 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
