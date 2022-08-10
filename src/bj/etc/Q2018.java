5456660399703fa770112e332e80026784e53f24
inhalin <inhalin.kim@gmail.com>
2022-08-10 23:20:35 +0900

    bj 2018 수들의 합 5

---
+++ src/bj/etc/Q2018.java
@@ -0,0 +1,20 @@
+package bj.etc;
+
+import java.util.Scanner;
+
+public class Q2018 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = 1, start = 1, end = 1, sum = 1;
+        while (end != n) {
+            if (sum > n) {
+                sum -= start++;
+            } else {
+                if (sum == n) count++;
+                sum += ++end;
+            }
+        }
+        System.out.println(count);
+    }
+}
